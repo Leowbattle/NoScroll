369ea04eb0f8466967c3eb2476fea0360d098aa7
leobattle <leobattle@sun>
2017-10-22 12:07:37 +0100

    Added increment and decrementing held item

--- src/main/java/noscroll/KeyBinds.java
+++ src/main/java/noscroll/KeyBinds.java
@@ -6,12 +6,15 @@ import net.minecraft.client.settings.KeyBinding;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 
 public class KeyBinds {
-	//This actually could be scroll lock if you want, but the default is control
-	public static KeyBinding scrollLock;
+	//This actually could be scroll lock if you want, but the default is backslash
+	public static KeyBinding scrollLock = new KeyBinding("key.scrolllock", Keyboard.KEY_BACKSLASH, "key.noscroll.category");;
+	
+	public static KeyBinding scrollUpOne = new KeyBinding("key.scrollupone", Keyboard.KEY_MINUS, "key.noscroll.category");
+	public static KeyBinding scrollDownOne = new KeyBinding("key.scrolldownone", Keyboard.KEY_EQUALS, "key.noscroll.category");
 	
 	public static void initKeyBinds() {
-		scrollLock = new KeyBinding("key.scrolllock", Keyboard.KEY_LCONTROL, "key.noscroll.category");
-		
 		ClientRegistry.registerKeyBinding(scrollLock);
+		ClientRegistry.registerKeyBinding(scrollUpOne);
+		ClientRegistry.registerKeyBinding(scrollDownOne);
 	}
 }
--- src/main/java/noscroll/KeyEvents.java
+++ src/main/java/noscroll/KeyEvents.java
@@ -1,13 +1,11 @@
 package noscroll;
 
-import org.lwjgl.input.Keyboard;
-
-import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.client.event.MouseEvent;
-import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
+import net.minecraftforge.fml.common.gameevent.InputEvent.KeyInputEvent;
 
 //The juice of the mod
 //Basically, if you want to see how the mod works, come here
@@ -21,6 +19,16 @@ public class KeyEvents {
 		//Scroll event
 		if (e.getDwheel() > 0 && !ModConfig.scrollToSelectIsEnabled && !KeyBinds.scrollLock.isKeyDown() || e.getDwheel() < 0 && !ModConfig.scrollToSelectIsEnabled && !KeyBinds.scrollLock.isKeyDown()) {
 			e.setCanceled(true);
+		}
+	}
+	
+	@SubscribeEvent
+	public static void keyEvent(KeyInputEvent e) {
+		if (KeyBinds.scrollUpOne.isPressed()) {
+			Minecraft.getMinecraft().player.inventory.changeCurrentItem(1);
+		}
+		if (KeyBinds.scrollDownOne.isPressed()) {
+			Minecraft.getMinecraft().player.inventory.changeCurrentItem(-1);
 		}
 	}
 }
